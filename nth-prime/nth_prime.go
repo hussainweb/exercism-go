96005a5b0fa1ed90bbc2f33d615c3dd2248d6782
hw <hussainweb@gmail.com>
2020-03-03 12:06:05 +0000

    Put in a shortcut

--- nth-prime/nth_prime.go
+++ nth-prime/nth_prime.go
@@ -23,6 +23,12 @@ func Nth(n int) (int, bool) {
 }
 
 func isPrime(p int) bool {
+	// Doesn't affect the benchmark much in terms of percentage,
+	// but this is a useful shortcut in other situations.
+	if p > 7 && (p%2 == 0 || p%3 == 0 || p%5 == 0 || p%7 == 0) {
+		return false
+	}
+
 	s := int(math.Sqrt(float64(p)))
 	for i := 2; i <= s; i++ {
 		if p%i == 0 {
