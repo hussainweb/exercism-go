b06c4e2d62129c53f16abdd25128411b10173b9d
hw <hussainweb@gmail.com>
2020-03-03 12:05:51 +0000

    Implement nth-prime

--- nth-prime/nth_prime.go
+++ nth-prime/nth_prime.go
@@ -1,1 +1,33 @@
 package prime
+
+import (
+	"math"
+)
+
+// Nth returns the n-th prime number
+func Nth(n int) (int, bool) {
+	if n == 0 {
+		return 0, false
+	}
+
+	foundPrimes := 0
+	for i := 2; ; i++ {
+		if isPrime(i) {
+			foundPrimes++
+			if foundPrimes == n {
+				// We have found our n-th prime
+				return i, true
+			}
+		}
+	}
+}
+
+func isPrime(p int) bool {
+	s := int(math.Sqrt(float64(p)))
+	for i := 2; i <= s; i++ {
+		if p%i == 0 {
+			return false
+		}
+	}
+	return true
+}
