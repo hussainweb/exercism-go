660dcc10b36ba4b32f0605e96f92eb0b95a67c94
hw <hussainweb@gmail.com>
2020-03-07 20:15:46 +0000

    Implement the solution for grains.

--- grains/grains.go
+++ grains/grains.go
@@ -1,1 +1,21 @@
 package grains
+
+import (
+	"errors"
+)
+
+// Square returns the number of grains on a specific square
+// of a chess-board
+func Square(square int) (uint64, error) {
+	if square <= 0 || square > 64 {
+		return 0, errors.New("Invalid size")
+	}
+
+	// Same as 2 ^ (square - 1)
+	return 1 << (square - 1), nil
+}
+
+// Total returns the number of grains for a chess-board
+func Total() uint64 {
+	return 1<<64 - 1
+}
