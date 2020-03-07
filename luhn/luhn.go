b9d1753efcbe8f055d7470ceb589b7e4b3c83b6f
hw <hussainweb@gmail.com>
2020-03-07 15:46:10 +0000

    Change how the doubling is decided in the luhn algorithm.

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -12,35 +12,31 @@ func Valid(sequence string) bool {
 		return false
 	}
 
-	// We need to double every digit from the RIGHT, but we can
-	// do it more efficiently by calculating the modulus to expect
-	// when scanning from the left.
-	// When the string has odd number of digits, it is every second
-	// digit from the left, but if there are even number of digits,
-	// it is every digit but starting at the first.
-	// We can do this by just calculating the modulus of 2 we
-	// should expect. For strings of even length, the modulus is 0
-	// which gets us every second digit starting from the first.
-	modulus := 1
-	if len(normalized)%2 == 0 {
-		modulus = 0
-	}
+	// We need to double every digit from the RIGHT, but scanning
+	// a string from the right isn't efficient. Hence, we set up
+	// a flag called 'double' which will indicate if the current
+	// digit should be doubled and we toggle that flag every loop.
+	// The initial value of the flag is dependent on if the string
+	// has even length or odd.
+	double := len(normalized)%2 == 0
 
 	checksum := 0
-	for i, l := range normalized {
+	for _, l := range normalized {
 		if !unicode.IsNumber(l) {
 			return false
 		}
 
 		// Convert the number in the character to an integer.
 		digit := int(l - '0')
-		if i%2 == modulus {
+		if double {
 			digit *= 2
 			if digit >= 10 {
 				digit -= 9
 			}
 		}
 		checksum += digit
+		// Toggle the 'doubling' of the next digit.
+		double = !double
 	}
 
 	return checksum%10 == 0
