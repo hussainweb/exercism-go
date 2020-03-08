9bc05250f059142d4860a0c31b23acdbac385903
hw <hussainweb@gmail.com>
2020-03-08 11:18:37 +0000

    Add implementation for clock

--- clock/clock.go
+++ clock/clock.go
@@ -1,1 +1,44 @@
 package clock
+
+import "fmt"
+
+// Clock represents a time object
+type Clock struct {
+	hour   int
+	minute int
+}
+
+// New creates a new clock
+func New(h int, m int) Clock {
+	for m < 0 {
+		m += 60
+		h--
+	}
+	for h < 0 {
+		h += 24
+	}
+
+	if m >= 60 {
+		h += m / 60
+		m %= 60
+	}
+	if h >= 24 {
+		h %= 24
+	}
+
+	return Clock{h, m}
+}
+
+func (c Clock) String() string {
+	return fmt.Sprintf("%02d:%02d", c.hour, c.minute)
+}
+
+// Add adds minutes to a clock
+func (c Clock) Add(minutes int) Clock {
+	return New(c.hour, c.minute+minutes)
+}
+
+// Subtract subtracts minutes from a clock
+func (c Clock) Subtract(minutes int) Clock {
+	return New(c.hour, c.minute-minutes)
+}
