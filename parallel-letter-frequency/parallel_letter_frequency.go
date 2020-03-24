967d70edb6e069119c57401cd46dd46fd8e44480
hw <hussainweb@gmail.com>
2020-03-23 22:49:45 -0400

    Implemented a solution for parallel frequency
    I also tried this without calling Frequency from the ConcurrentFrequency function and directly looping over the string. I thought this would be faster because 1) we are avoiding two for loops and 2) we are not calling Frequency anymore. However, it was at least twice as slow. This is where the performance effects of mutexes and benefits of sync.Map() come up. However, I can't think of how to use sync.Map() without mutexes without changing the signature of Frequency(). And I can't change that because the test calls that function.

--- parallel-letter-frequency/parallel_letter_frequency.go
+++ parallel-letter-frequency/parallel_letter_frequency.go
@@ -1,5 +1,7 @@
 package letter
 
+import "sync"
+
 // FreqMap records the frequency of each rune in a given text.
 type FreqMap map[rune]int
 
@@ -11,4 +13,39 @@ func Frequency(s string) FreqMap {
 		m[r]++
 	}
 	return m
+}
+
+// ConcurrentFrequency counts the frequency of each rune in parallel.
+func ConcurrentFrequency(strings []string) FreqMap {
+	var wg sync.WaitGroup
+	sc := safeCounter{}
+	sc.v = make(FreqMap)
+
+	for _, s := range strings {
+		wg.Add(1)
+		go func(str string) {
+			smap := Frequency(str)
+			for r, c := range smap {
+				sc.Add(r, c)
+			}
+			defer wg.Done()
+		}(s)
+	}
+
+	wg.Wait()
+
+	return sc.v
+}
+
+type safeCounter struct {
+	v   FreqMap
+	mux sync.Mutex
+}
+
+// Add increases the value for the counter for the given key.
+func (c *safeCounter) Add(key rune, count int) {
+	c.mux.Lock()
+	// Lock so only one goroutine at a time can access the map c.v.
+	c.v[key] += count
+	c.mux.Unlock()
 }
