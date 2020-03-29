84eec0892772e9b35130d227f95954780e4764b2
hw <hussainweb@gmail.com>
2020-03-29 11:40:18 -0400

    Use a channel of FreqMap instead of runeCounter

--- parallel-letter-frequency/parallel_letter_frequency.go
+++ parallel-letter-frequency/parallel_letter_frequency.go
@@ -5,11 +5,6 @@ import "sync"
 // FreqMap records the frequency of each rune in a given text.
 type FreqMap map[rune]int
 
-type runeCount struct {
-	r rune
-	c int
-}
-
 // Frequency counts the frequency of each rune in a given text and returns this
 // data as a FreqMap.
 func Frequency(s string) FreqMap {
@@ -22,40 +17,28 @@ func Frequency(s string) FreqMap {
 
 // ConcurrentFrequency counts the frequency of each rune in parallel.
 func ConcurrentFrequency(strings []string) FreqMap {
-	var runeCounterChan = make([]chan runeCount, len(strings))
-
-	for i, s := range strings {
-		runeCounterChan[i] = make(chan runeCount, 25)
-		go func(str string, runeCounterChan chan runeCount) {
-			smap := Frequency(str)
-			for r, c := range smap {
-				runeCounterChan <- runeCount{r: r, c: c}
-			}
-			close(runeCounterChan)
-		}(s, runeCounterChan[i])
-	}
-
 	var wg sync.WaitGroup
-	var merged = make(chan runeCount, 100)
-	for i := range strings {
+	var freqMapChan = make(chan FreqMap, len(strings))
+
+	for _, s := range strings {
 		wg.Add(1)
-		go func(i int) {
-			for rc := range runeCounterChan[i] {
-				merged <- rc
-			}
+		go func(str string) {
+			freqMapChan <- Frequency(str)
 			defer wg.Done()
-		}(i)
+		}(s)
 	}
 
 	go func() {
 		wg.Wait()
-		close(merged)
+		close(freqMapChan)
 	}()
 
 	fm := FreqMap{}
-	for rc := range merged {
+	for smap := range freqMapChan {
 		// Process each value
-		fm[rc.r] += rc.c
+		for r, c := range smap {
+			fm[r] += c
+		}
 	}
 
 	return fm
