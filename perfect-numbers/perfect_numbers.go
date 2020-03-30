9562755f065e428314062a2cb960472f7aee7ed7
hw <hussainweb@gmail.com>
2020-03-30 10:31:00 -0400

    Implemented solution for perfect-numbers

--- perfect-numbers/perfect_numbers.go
+++ perfect-numbers/perfect_numbers.go
@@ -1,1 +1,49 @@
 package perfect
+
+import "errors"
+
+// ErrOnlyPositive indicates that the number should be positive
+var ErrOnlyPositive error = errors.New("Only positive numbers allowed")
+
+// Classification represents the number's classification
+type Classification int
+
+// Different classifications
+const (
+	_ = iota
+	ClassificationPerfect
+	ClassificationAbundant
+	ClassificationDeficient
+)
+
+// Classify checks if a number is perfect, abundant, or deficient.
+func Classify(number int64) (classification Classification, err error) {
+	if number <= 0 {
+		err = ErrOnlyPositive
+		return
+	}
+
+	aliquotSum := aliquotSum(number)
+	switch {
+	case aliquotSum == number:
+		classification = ClassificationPerfect
+	case aliquotSum > number:
+		classification = ClassificationAbundant
+	case aliquotSum < number:
+		classification = ClassificationDeficient
+	}
+
+	return
+}
+
+func aliquotSum(number int64) int64 {
+	sum := int64(0)
+
+	for i := int64(1); i < number/2+1; i++ {
+		if number%i == 0 {
+			sum += i
+		}
+	}
+
+	return sum
+}
