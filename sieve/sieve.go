bef3ed02a179db850e9060e7b4bb065043b14469
hw <hussainweb@gmail.com>
2020-03-30 09:37:38 -0400

    Implemented solution for sieve

--- sieve/sieve.go
+++ sieve/sieve.go
@@ -1,1 +1,28 @@
 package sieve
+
+// Sieve finds prime numbers up to a limit.
+func Sieve(limit int) []int {
+	var primes []int
+	candidates := make(map[int]bool)
+
+	for i := 2; i <= limit; i++ {
+		isPrime, ok := candidates[i]
+		if ok && !isPrime {
+			// If the key exists and is false, this number is already
+			// marked as not-prime.
+			continue
+		}
+
+		// Mark the number as prime and mark multiples as not prime.
+		candidates[i] = true
+		primes = append(primes, i)
+
+		n := 2
+		for n*i <= limit {
+			candidates[n*i] = false
+			n++
+		}
+	}
+
+	return primes
+}
