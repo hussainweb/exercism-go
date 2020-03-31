0e588f88b93cd928d31269f6b4a981efe8ff67c8
hw <hussainweb@gmail.com>
2020-03-31 17:21:14 -0400

    Implemented solution for armstrong-numbers

--- armstrong-numbers/armstrong_numbers.go
+++ armstrong-numbers/armstrong_numbers.go
@@ -1,1 +1,18 @@
 package armstrong
+
+import (
+	"math"
+	"strconv"
+)
+
+// IsNumber checks if the number is armstrong.
+func IsNumber(n int) bool {
+	power := float64(len(strconv.Itoa(n)))
+	sum := 0
+	x := n
+	for x > 0 {
+		sum += int(math.Pow(float64(x%10), power))
+		x /= 10
+	}
+	return sum == n
+}
