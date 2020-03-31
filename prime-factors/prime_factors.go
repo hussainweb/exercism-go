2744687ad00a7bfa5e0e8ee035212a8ac004bc26
hw <hussainweb@gmail.com>
2020-03-31 14:13:36 -0400

    Implemented solution for prime-factors

--- prime-factors/prime_factors.go
+++ prime-factors/prime_factors.go
@@ -1,1 +1,20 @@
 package prime
+
+// Factors returns the prime factors.
+func Factors(n int64) []int64 {
+	factors := make([]int64, 0)
+
+	remainingProduct := n
+	for i := int64(2); i <= n; i++ {
+		for remainingProduct%i == 0 {
+			factors = append(factors, i)
+			remainingProduct /= i
+		}
+		// If we have reached 1 as our last factor, we're done.
+		if remainingProduct == 1 {
+			break
+		}
+	}
+
+	return factors
+}
