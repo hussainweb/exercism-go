603c6f5bcbe352b7af9fdd13205ccf92120d444b
hw <hussainweb@gmail.com>
2020-04-05 14:23:52 -0400

    Combined the validation loop into the main tree-building loop

--- tree-building/tree_building.go
+++ tree-building/tree_building.go
@@ -18,16 +18,9 @@ type Node struct {
 }
 
 // Build constructs a tree from a set of records.
-func Build(records []Record) (root *Node, err error) {
+func Build(records []Record) (*Node, error) {
 	if len(records) == 0 {
-		root = nil
-		return
-	}
-
-	// Let's validate first.
-	err = validateRecords(records)
-	if err != nil {
-		return nil, err
+		return nil, nil
 	}
 
 	sort.Slice(records, func(i, j int) bool {
@@ -35,15 +28,38 @@ func Build(records []Record) (root *Node, err error) {
 	})
 
 	nodes := make(map[int]*Node)
+	largestID := -1
 	for _, r := range records {
-		nodes[r.ID] = &Node{ID: r.ID}
-		if r.ID == 0 {
-			continue
+		if _, ok := nodes[r.ID]; ok {
+			return nil, errors.New("Duplicate record")
+		}
+		if r.ID == 0 && r.Parent != 0 {
+			return nil, errors.New("Invalid root")
+		}
+		if r.ID != 0 && r.Parent >= r.ID {
+			return nil, errors.New("Parent cannot be larger than or same as ID")
 		}
 
-		parentNode := nodes[r.Parent]
-		currentNode := nodes[r.ID]
-		parentNode.Children = append(parentNode.Children, currentNode)
+		nodes[r.ID] = &Node{ID: r.ID}
+		if r.ID > largestID {
+			largestID = r.ID
+		}
+
+		if r.ID != 0 {
+			parentNode, parentFound := nodes[r.Parent]
+			if !parentFound {
+				return nil, errors.New("parent node not found; nodes may not be sequential")
+			}
+			parentNode.Children = append(parentNode.Children, nodes[r.ID])
+		}
+	}
+
+	if _, ok := nodes[0]; !ok {
+		return nil, errors.New("No root node")
+	}
+
+	if largestID+1 != len(nodes) {
+		return nil, errors.New("The IDs are not sequential")
 	}
 
 	return nodes[0], nil
