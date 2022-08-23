0a30462d97062a91daac36309576c15f5d5b79cf
hw <hussainweb@gmail.com>
2022-08-23 15:21:57 -0400

    solve interest-is-interesting

--- interest-is-interesting/interest_is_interesting.go
+++ interest-is-interesting/interest_is_interesting.go
@@ -1,21 +1,41 @@
 package interest
 
 // InterestRate returns the interest rate for the provided balance.
-func InterestRate(balance float64) float32 {
-	panic("Please implement the InterestRate function")
+func InterestRate(balance float64) float64 {
+	switch {
+	case balance < 0:
+		return 3.213
+	case balance >= 0 && balance < 1000:
+		return 0.5
+	case balance >= 1000 && balance < 5000:
+		return 1.621
+	default:
+		return 2.475
+	}
 }
 
 // Interest calculates the interest for the provided balance.
 func Interest(balance float64) float64 {
-	panic("Please implement the Interest function")
+	return balance * (InterestRate(balance) / 100)
 }
 
 // AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
 func AnnualBalanceUpdate(balance float64) float64 {
-	panic("Please implement the AnnualBalanceUpdate function")
+	return balance + Interest(balance)
 }
 
 // YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance.
 func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
-	panic("Please implement the YearsBeforeDesiredBalance function")
+	// We could use Compound Interest formula to get the answer in this way:
+	// int(math.Ceil(math.Log(targetBalance/balance) / math.Log(1.0+(InterestRate(balance)/100))))
+	//
+	// However, this won't account for the fact that the interest rate will change in some cases.
+	// So, we have to do this with a loop.
+	years := 0
+	thisYearBalance := balance
+	for thisYearBalance < targetBalance {
+		thisYearBalance += Interest(thisYearBalance)
+		years++
+	}
+	return years
 }
