0153122d6ac7c87a667f51b15b5447e457649497
hw <hussainweb@gmail.com>
2022-08-25 15:39:24 -0400

    solve lasagna-master

--- lasagna-master/lasagna_master.go
+++ lasagna-master/lasagna_master.go
@@ -1,9 +1,34 @@
 package lasagna
 
-// TODO: define the 'PreparationTime()' function
+func PreparationTime(slices []string, averageTime int) int {
+	if averageTime == 0 {
+		averageTime = 2
+	}
+	return len(slices) * averageTime
+}
 
-// TODO: define the 'Quantities()' function
+func Quantities(slices []string) (int, float64) {
+	noodles := 0
+	sauce := 0.0
+	for _, slice := range slices {
+		switch slice {
+		case "noodles":
+			noodles += 50
+		case "sauce":
+			sauce += 0.2
+		}
+	}
+	return noodles, sauce
+}
 
-// TODO: define the 'AddSecretIngredient()' function
+func AddSecretIngredient(friendsList []string, myList []string) {
+	myList[len(myList)-1] = friendsList[(len(friendsList) - 1)]
+}
 
-// TODO: define the 'ScaleRecipe()' function
+func ScaleRecipe(portions []float64, numPortions int) []float64 {
+	newPortions := make([]float64, len(portions))
+	for i := range newPortions {
+		newPortions[i] = portions[i] * float64(numPortions) / 2
+	}
+	return newPortions
+}
